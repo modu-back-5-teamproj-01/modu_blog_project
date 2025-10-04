3ae8a0b35c21dca0931d013ac69ca102dbc8158d
nockbeet <skcheon2001@gmail.com>
2025-10-04 15:14:41 +0900

    feat: sqlalchemy 모델 수정

--- back/app/models/post.py
+++ back/app/models/post.py
@@ -1,15 +1,15 @@
 import datetime
-from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, Text
 from sqlalchemy.orm import relationship
 from .base import Base
 
 class Post(Base):
     __tablename__ = "posts"
     id = Column(Integer, primary_key=True, index=True)
-    author_id = Column(Integer, ForeignKey("users.id"))
-    title = Column(String)
-    content = Column(String)
-    summary = Column(String)
+    author_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    title = Column(String, nullable=False)
+    content = Column(Text, nullable=False)
+    summary = Column(Text)
     view_count = Column(Integer, default=0)
     created_at = Column(DateTime, default=datetime.datetime.now)
     updated_at = Column(DateTime, default=datetime.datetime.now, onupdate=datetime.datetime.now)
--- back/app/models/tag.py
+++ back/app/models/tag.py
@@ -6,6 +6,6 @@ class Tag(Base):
     __tablename__ = "tags"
 
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, unique=True, nullable=False)
+    name = Column(String, unique=True, nullable=False)  # 필요 시 길이 제한할 것
 
     posts = relationship("PostTag", back_populates="tag")
